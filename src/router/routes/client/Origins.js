814d4e84fedeac7b763c959e771adce166e04e9e
= <lorenzoabreu.dev@gmail.com>
2023-10-20 20:27:25 -0300

    bora

--- src/router/routes/client/Origins.js
+++ src/router/routes/client/Origins.js
@@ -1,27 +1,35 @@
-const router = require('express').Router()
-const OriginController = require('../../../database/controllers/OriginController')
-const status = require('../../../functions/status')
-const Origin = new OriginController()
+const router = require("express").Router();
+const OriginController = require("../../../database/controllers/OriginController");
+const status = require("../../../functions/status");
+const Origin = new OriginController();
 
-router.route('/client/origins')
-    .post(async (req, res) => {
-        const {
-            title,
-            url
-        } = req.body
+router
+  .route("/client/origins")
+  .get(async (req, res) => {
+    try {
+      console.log(req._username);
+      const result = await Origin.GetAll(req._username);
+      return res.json({
+        status: 200,
+        data: result || [],
+      });
+    } catch {
+      return res.json(status.server_error);
+    }
+  })
+  .post(async (req, res) => {
+    const { title, url } = req.body;
 
-        if (!req._username) return status.user_not_found
+    if (!req._username) return res.json(status.user_not_found);
 
-        Origin.Create(req._username, title, url)
-            .then((result) => {
-                res.json(result) 
-            })
-            .catch(() => {
-                return status.server_error
-            })
-        })
-    .put((req, res) => {
+    Origin.Create(req._username, title, url)
+      .then((result) => {
+        return res.json(result);
+      })
+      .catch(() => {
+        return res.json(status.server_error);
+      });
+  })
+  .put((req, res) => {});
 
-    })
-
-module.exports = router
+module.exports = router;
