946df2008960d5caad54cf63170fe84f6fb5104d
= <lorenzoabreu.dev@gmail.com>
2023-10-20 22:56:18 -0300

    bora

--- src/router/routes/auth/Login.js
+++ src/router/routes/auth/Login.js
@@ -1,20 +1,17 @@
-const router = require('express').Router()
-const UserController = require('../../../database/controllers/UserController')
-const User = new UserController()
-const status = require('../../../functions/status')
+const router = require("express").Router();
+const UserController = require("../../../database/controllers/UserController");
+const User = new UserController();
+const status = require("../../../functions/status");
 
-router.post('/auth/login', async (req, res) => {
-    const {
-        username,
-        password
-    } = req.body
+router.post("/auth/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(req.body);
+  try {
+    const result = await User.Login(username, password);
+    res.json(result);
+  } catch {
+    res.json(status.server_error);
+  }
+});
 
-    try {
-        const result = await User.Login(username, password)
-        res.json(result)
-    } catch {
-        res.json(status.server_error)
-    }
-})
-
-module.exports = router
+module.exports = router;
