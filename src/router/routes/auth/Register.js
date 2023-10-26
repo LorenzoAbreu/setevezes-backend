bf3613200ec1d9b1777721e76bcbba2895537a1c
= <lorenzoabreu.dev@gmail.com>
2023-10-26 10:49:58 -0300

    aaaaaaaaaa

--- src/router/routes/auth/Register.js
+++ src/router/routes/auth/Register.js
@@ -4,20 +4,21 @@ const User = new UserController();
 
 router.post("/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
+  console.log(req.body);
 
   try {
     const result = await User.Create(username, email, password);
     console.log(result);
 
-    if (result.status == 20) {
-      res.json(result);
+    if (result.status == 200) {
+      return res.json(result);
     } else {
       const { username, email, password } = req.body.data;
       const newResult = await User.Create(username, email, password);
-      res.json(newResult);
+      return res.json(newResult);
     }
   } catch {
-    res.json({
+    return res.json({
       status: 500,
       error: "Erro no servidor!",
     });
