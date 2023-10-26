ddcc47dbece7bba07cd0f3e16faef37c3a5255e3
= <lorenzoabreu.dev@gmail.com>
2023-10-26 18:08:03 -0300

    afjaifjisa

--- src/router/routes/admin/Users.js
+++ src/router/routes/admin/Users.js
@@ -5,15 +5,14 @@ const router = require("express").Router();
 const UserController = require("../../../database/controllers/UserController");
 const user = new UserController();
 
-router
-  .route("/admin/users/:username")
-  .put(async (req, res) => {
-    const result = await user.Edit(req.params.username, req.body);
-    res.json(result);
-  })
-  .get(async (req, res) => {
-    const result = await user.getAll();
-    return res.json(result);
-  });
+router.route("/admin/users/:username").put(async (req, res) => {
+  const result = await user.Edit(req.params.username, req.body);
+  res.json(result);
+});
+
+router.get("/admin/users", async (req, res) => {
+  const result = await user.getAll();
+  return res.json(result);
+});
 
 module.exports = router;
